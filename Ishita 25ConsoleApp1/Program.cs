db5fd237878cd23fb11cdfab15345edb30d6ebb6
A <A@ANUJ>
2023-10-04 22:22:22 -0400

    Add project files.

---
+++ Ishita 25ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_25ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] strings = { "Actions speak louder than words", "Hello!", "Python.",
+                           "PHP.", "random" };
+            foreach (var value in strings)
+            {
+                bool endsInPeriod = value.EndsWith(".");
+                Console.WriteLine();
+                Console.WriteLine("'{0}' ends in a period: {1}",
+                                  value, endsInPeriod);
+            }
+        }
+    }
+}
+    
